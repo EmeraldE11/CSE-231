d6dc60e0d4a565397279537964b6949bc7f375fe
Spencer Palmer <monkies989@gmail.com>
2026-01-30 17:00:35 -0700

    Completed Ticket 5

--- W4/231.04.Lab/board.cpp
+++ W4/231.04.Lab/board.cpp
@@ -40,10 +40,31 @@ void Board::reset(bool fFree)
    for (int r = 0; r < 8; r++)
       for (int c = 0; c < 8; c++)
          board[c][r] = nullptr;
+   
+   // White Pieces
+//   board[0][0] = new Rook(0, 0, true);
+   board[1][0] = new Knight(1, 0, true);
+//   board[2][0] = new Bishop(2, 0, true);
+//   board[3][0] = new Queen(3, 0, true);
+//   board[4][0] = new King(4, 0, true);
+//   board[5][0] = new Bishop(5, 0, true);
+   board[6][0] = new Knight(6, 0, true);
+//   board[7][0] = new Rook(7, 0, true);
+
+   // Black Pieces
+//   board[0][7] = new Rook(0, 7, false);
+   board[1][7] = new Knight(1, 7, false);
+//   board[2][7] = new Bishop(2, 7, false);
+//   board[3][7] = new Queen(3, 7, false);
+//   board[4][7] = new King(4, 7, false);
+//   board[5][7] = new Bishop(5, 7, false);
+   board[6][7] = new Knight(6, 7, false);
+//   board[7][7] = new Rook(7, 7, false);
+   
 }
 
 // we really REALLY need to delete this.
-Space space(0,0);
+//Space space(0,0);
 
 /***********************************************
 * BOARD : GET
@@ -76,7 +97,8 @@ void Board::display(const Position & posHover, const Position & posSelect) const
  ************************************************/
 Board::Board(ogstream* pgout, bool noreset) : pgout(pgout), numMoves(0)
 {
-
+   if (!noreset)
+      reset(false);
 }
 
 
@@ -110,6 +132,26 @@ void Board::assertBoard()
 void Board::move(const Move & move)
 {  
 
+   // increment move counter
+   numMoves++;
+   
+   Position src = move.getSrc();
+   Position des = move.getDes();
+
+   int srcCol = src.getCol();
+   int srcRow = src.getRow();
+   int dstCol = des.getCol();
+   int dstRow = des.getRow();
+
+   // create move pointer
+   Piece* pMoving = board[srcCol][srcRow];
+
+   // destination becomes moving piece
+   board[dstCol][dstRow] = pMoving;
+
+   // source becomes SPACE
+   board[srcCol][srcRow] = new Space(srcCol, srcRow);
+   
 }
 
 
